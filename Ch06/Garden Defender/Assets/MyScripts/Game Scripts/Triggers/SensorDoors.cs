866f18c7df91c5c75562de9522ed298441ce374d
DaveLehman <lemo@provide.net>
2021-04-21 19:17:32 -0400

    Animated Assets and Textures, Garden Gates Prefab

---
+++ Ch06/Garden Defender/Assets/MyScripts/Game Scripts/Triggers/SensorDoors.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SensorDoors : MonoBehaviour
+{
+	public AnimationClip clipOpen; // the open animation
+	public AnimationClip clipClose; // the close animation
+
+
+	// open the gates
+	void OnTriggerEnter(Collider defender)
+	{
+		print("Trigger Entered");
+		if (defender.gameObject.tag == "Player")
+		{
+			print("by player");
+			GetComponent<Animation>().Play(clipOpen.name);
+			GetComponent<AudioSource>().Play(); // play the clip loaded in the audio component	
+		}
+	}
+
+	// close the gates
+	void OnTriggerExit(Collider defender)
+	{
+		print("Trigger Exited");
+		if (defender.gameObject.tag == "Player")
+		{
+			print("by player");
+			GetComponent<Animation>().Play(clipClose.name);
+			GetComponent<AudioSource>().Play(); // play the clip loaded in the audio component
+		}
+	}
+}
